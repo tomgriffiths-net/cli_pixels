0209f347319b68ee007e0128e149db3f13142854
Tom <tomgriffiths2007@gmail.com>
2025-06-15 20:16:40 +0100

    Updated everything

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # cli_pixels
-This is a package for PHP-CLI.
+cli_pixels is a PHP-CLI package that provides functionality to address pixels in the command line as if it were a computer screen.
 
-# Use
-To use this program in your own project:
-* Link back to original source (here or www.tomgriffiths.net).
-* Do not claim this code as your own.
+# Commands
+- **grad**: Displays an animated gradient for 10 seconds.
+
+# Functions
+- **set_screen_size(int $x=48, int $y=27, bool $apply=true):bool**: Sets the command window size to the specified width and height. Apply does not work on windows 11 so the user will have to resize the window themselves or a size smaller than 61x30 should be specified. Returns true on success and false on failure.
+
+- **set_pixel(array $coordinates=['x'=>0,'y'=>0], array $data=['r'=>50,'g'=>50,'b'=>50]):bool**: Sets a specific pixel to a specific colour determined by the rgb data. Returns true on success and false on failure.
+
+- **push(array|bool $pixelsArray=false):bool**: Pushes the current pixel data to the display, $pixelsArray can be set to a custom pixel array to push custom data, incorrect data may cause issues. Returns true on success and false on failure.
+
+- **push(array|bool $pixelsArray=false):bool**: Pushes the current pixel data to the display, $pixelsArray can be set to a custom pixel array to push custom data, incorrect data may cause issues. Returns true on success and false on failure.
+
+- **fill(array $data = ['r'=>50,'g'=>50,'b'=>50]):bool**: Sets all pixels to the same colour. Returns true on success or false on failure.
+
+- **showRgbFrame(string $data, int $width=69, int $height=39):bool**: Takes in binary data (24 bit rgb) and displays it on the screen. Returns true on success and false on failure.
+
+- **playVideo(string $input, int $width=69, int $height=39):bool**: Plays a video in the command line. Returns true on success and false on failure.
+
+- **line(array $points, array $pixelData):bool**: Creates a line in the global pixel buffer, where each pixel has the provided pixel data, the $points variable takes an array consisting of 2 points with x and y positions such as this: ['x1'=>1,'y1'=>1,'x2'=>10,'y2=>15']. Returns true on success and false on failure.
+
+- **circle(int $centerX, int $centerY, int $radius, array $pixelData):bool**: Creates a circle in the global pixel buffer with the provided pixel data. Returns true on success and false on failure.
--- main.php
+++ main.php
@@ -1,5 +1,8 @@
 <?php
 class cli_pixels{
+    private static $screenSize = ['x'=>60,'y'=>30];
+    private static $pixels = [];
+
     public static function command($line){
         $lines = explode(" ",$line);
         if($lines[0] === "grad"){
@@ -11,7 +14,10 @@ class cli_pixels{
             $x = 48;
             $y = 27;
             $offset = 0;
-            while(true){
+
+            $totalFrames = 300; //10 seconds
+            $frames = 0;
+            while($frames < $totalFrames){
                 
                 for($yi=0; $yi<$y+1; $yi++){
 
@@ -36,19 +42,216 @@ class cli_pixels{
 
                 self::push();
                 
-                usleep(33000);
+                usleep(28000);//Delay left for 1 frame to be 33ms
+                $frames++;
                 //return;
             }
         }
     }
-    public static function line(array $points, array $data){
-        $lineCoordinates = self::internal_lineCoords($points['x1'], $points['y1'], $points['x2'], $points['y2']);
-        foreach ($lineCoordinates as $coord) {
-            list($x, $y) = $coord;
-            self::set_pixel(array('x'=>$x,'y'=>$y),$data);
+
+    public static function set_screen_size(int $x = 48, int $y = 27, bool $apply = true):bool{
+        if($x < 0 | $x > 800){
+            return false;
         }
+        if($y < 0 | $y > 450){
+            return false;
+        }
+
+        self::$screenSize['x'] = $x;
+        self::$screenSize['y'] = $y;
+
+        if($apply){
+            exec('mode con: cols=' . $x*2 . ' lines=' . $y+1);
+        }
+
+        return true;
     }
-    private static function internal_lineCoords($x1, $y1, $x2, $y2){
+    public static function set_pixel(array $coordinates = ['x'=>0,'y'=>0], array $data = ['r'=>50,'g'=>50,'b'=>50]):bool{
+        foreach(['x','y'] as $coord){
+            if($coordinates[$coord] > self::$screenSize[$coord]-1){
+                return false;
+            }
+            if($coordinates[$coord] < 0){
+                return false;
+            }
+        }
+
+        if(!self::isValidPixelData($data)){
+            return false;
+        }
+
+        self::$pixels[$coordinates['y']][$coordinates['x']] = $data;
+        
+        return true;
+    }
+    public static function push(array|bool $pixelsArray = false):bool{
+        if($pixelsArray === false){
+            if(!isset(self::$pixels) || !is_array(self::$pixels)){
+                return false;
+            }
+            $push = self::$pixels;
+        }
+        else{
+            $push = $pixelsArray;
+        }
+
+        //self::$pixels[39][100]['r/g/b']
+        $output = "";
+        foreach($push as $rowNum => $columns){
+            if(!is_array($columns)){
+                return false;
+            }
+            foreach($columns as $colNum=> $data){
+                if(!is_array($data)){
+                    return false;
+                }
+                $output .= "\033[48;2;" . round($data['r'] ?? 0) . ";" . round($data['g'] ?? 0) . ";" . round($data['b'] ?? 0) . "m  \033[0m";
+            }
+            $output .= "\n";
+        }
+        echo "\033[?25l\033[H";
+        echo $output;
+        echo "\033[?25h";
+
+        return true;
+    }
+    public static function fill(array $data = ['r'=>50,'g'=>50,'b'=>50]):bool{
+        if(!self::isValidPixelData($data)){
+            return false;
+        }
+
+        $row = 0;
+        while($row < self::$screenSize['y']){
+            $col = 0;
+            while($col < self::$screenSize['x']){
+                self::$pixels[$row][$col] = $data;
+                $col++;
+            }
+            $row++;
+        }
+
+        return true;
+    }
+    private static function isValidPixelData(array $data):bool{
+        foreach(['r','g','b'] as $ch){
+            if(!isset($data[$ch])){
+                return false;
+            }
+            if(!is_int($data[$ch])){
+                return false;
+            }
+            if($data[$ch] < 0 || $data[$ch] > 255){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    public static function showRgbFrame(string $data, int $width=69, int $height=39):bool{
+        if(strlen($data) === $width * $height * 3){
+            // Convert data to pixels format
+            $pixels = [];
+            for($pixRow = 0; $pixRow < $height; $pixRow++){
+                $pixels[$pixRow] = [];
+                
+                for($pixCol = 0; $pixCol < $width; $pixCol++){
+                    $pixelIndex = ($pixRow * $width + $pixCol) * 3;
+
+                    // Extract RGB values (each is 1 byte)
+                    $pixels[$pixRow][$pixCol] = [
+                        'r' => ord($data[$pixelIndex] ?? '0'), // string 0 => int 48
+                        'g' => ord($data[$pixelIndex + 1] ?? '0'),
+                        'b' => ord($data[$pixelIndex + 2] ?? '0')
+                    ];
+                }
+            }
+
+            return self::push($pixels);
+        }
+
+        return false;
+    }
+    public static function playVideo(string $input, int $width = 69, int $height = 39):bool{
+        if(!file_exists($input)){
+            echo "Input file does not exist\n";
+            return false;
+        }
+
+        if(!class_exists('e_ffmpeg')){
+            echo "FFmpeg not installed, please run: pkgmgr install e_ffmpeg\n";
+            return false;
+        }
+
+        $ffmpeg = e_ffmpeg::path();
+        if(!is_string($ffmpeg)){
+            echo "Failed to locate ffmpeg.exe, is it installed?\n";
+            return false;
+        }
+
+        $bytesPerFrame = $width * $height * 3;
+
+        if($bytesPerFrame > 8192){//Max pipe buffer
+            echo "Specified frame size is too large\n";
+            return false;
+        }
+        
+        $ffmpegCmd = $ffmpeg . ' -re -i ' .  escapeshellarg($input) . ' -filter:v "scale=' . $width . ':' . $height . '" -f rawvideo -pix_fmt rgb24 -loglevel quiet pipe:1';
+        
+        // Open FFmpeg process with pipes
+        $process = proc_open($ffmpegCmd, [['pipe', 'r'],['pipe','w']], $pipes);
+        
+        if(!is_resource($process)){
+            echo "Failed to open ffmpeg process\n";
+            return false;
+        }
+        
+        self::set_screen_size($width,$height);
+        
+        while(true){
+            // Read from FFmpeg stdout
+            $data = fread($pipes[1], $bytesPerFrame);
+            if($data !== false){
+                if(!self::showRgbFrame($data, $width, $height)){
+                    echo "No more data\n";
+                    break;
+                }
+            }
+        }
+
+        // Close
+        fclose($pipes[0]);
+        $exitCode = proc_close($process);
+        
+        if($exitCode !== 0){
+            echo "Ffmpeg did not exit properly\n";
+            return false;
+        }
+        
+        return true;
+    }
+
+    public static function line(array $points, array $pixelData):bool{
+        foreach(['x1','y1','x2','y2'] as $point){
+            if(!isset($points[$point])){
+                return false;
+            }
+            if(!is_int($points[$point])){
+                return false;
+            }
+        }
+
+        $lineCoordinates = self::internal_lineCoords($points['x1'], $points['y1'], $points['x2'], $points['y2']);
+        foreach($lineCoordinates as $coord){
+            list($x, $y) = $coord;
+            if(!self::set_pixel(array('x'=>$x,'y'=>$y),$pixelData)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+    private static function internal_lineCoords(int $x1, int $y1, int $x2, int $y2):array{
         $coordinates = array();
     
         $dx = abs($x2 - $x1);
@@ -81,15 +284,22 @@ class cli_pixels{
     
         return $coordinates;
     }
-    public static function circle($centerX, $centerY, $radius, $pixelData){
-        
+    public static function circle(int $centerX, int $centerY, int $radius, array $pixelData):bool{
+        if(!self::isValidPixelData($pixelData)){
+            return false;
+        }
+
         $circleCoordinates = self::internal_circleCoords($centerX, $centerY, $radius);
         foreach ($circleCoordinates as $coord) {
             list($x, $y) = $coord;
-            self::set_pixel(array('x'=>$x,'y'=>$y),$pixelData);
+            if(!self::set_pixel(array('x'=>$x,'y'=>$y),$pixelData)){
+                return false;
+            }
         }
+
+        return true;
     }
-    private static function internal_circleCoords($centerX, $centerY, $radius){
+    private static function internal_circleCoords(int $centerX, int $centerY, int $radius):array{
         $coordinates = array();
 
         $x = $radius - 1;
@@ -122,73 +332,5 @@ class cli_pixels{
         }
 
         return $coordinates;
-    }
-    public static function set_pixel(array $coordinates = array('x'=>0,'y'=>0), array $data = array('r'=>50,'g'=>50,'b'=>50)):bool{
-        $success = false;
-        $setpixel = true;
-        
-        foreach(array('x','y') as $coord){
-            if($coordinates[$coord] > $GLOBALS['cli_pixels']['screen_size'][$coord]-1){
-                $setpixel = false;
-            }
-            if($coordinates[$coord] < 0){
-                $setpixel = false;
-            }
-        }
-
-        if($setpixel){
-            $GLOBALS['cli_pixels']['pixels'][$coordinates['y']][$coordinates['x']] = $data;
-        }
-        $success = true;
-        return $success;
-    }
-    public static function set_screen_size(int $x = 48, int $y = 27, bool $apply = true){
-        $GLOBALS['cli_pixels']['screen_size']['x'] = $x;
-        $GLOBALS['cli_pixels']['screen_size']['y'] = $y;
-        if($apply){
-            exec('mode con: cols=' . $x*2 . ' lines=' . $y+1);
-        }
-    }
-    public static function push(array|bool $pixelsArray = false):bool{
-        $success = false;
-        if($pixelsArray === false){
-            if(isset($GLOBALS['cli_pixels']['pixels'])){
-                $push = $GLOBALS['cli_pixels']['pixels'];
-            }
-            else{
-                goto end;
-            }
-        }
-        else{
-            $push = $pixelsArray;
-        }
-
-        if(is_array($push)){
-            //$GLOBALS['cli_pixels']['pixels'][39][100]['r/g/b']
-            $output = "";
-            foreach($push as $row => $columns){
-                foreach($columns as $column => $data){
-                    $output .= "\033[48;2;" . round($data['r']) . ";" . round($data['g']) . ";" . round($data['b']) . "m  \033[0m";
-                }
-                $output .= "\n";
-            }
-            echo "\033[?25l\033[H";
-            echo $output;
-            echo "\033[?25h";
-            $success = true;
-        }
-        end:
-        return $success;
-    }
-    public static function fill(array $data = array('r'=>50,'g'=>50,'b'=>50)){
-        $row = 0;
-        while($row < $GLOBALS['cli_pixels']['screen_size']['y']){
-            $col = 0;
-            while($col < $GLOBALS['cli_pixels']['screen_size']['x']){
-                $GLOBALS['cli_pixels']['pixels'][$row][$col] = $data;
-                $col++;
-            }
-            $row++;
-        }
     }
 }
